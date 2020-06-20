c05e5528632f40f652ce74c3e875c8d8c5abc989
aaronl647 <AaronLei@Aarons-MacBook-Pro.local>
2020-06-20 17:28:40 -0400

    Updates

---
+++ precache-manifest.b4472fd7ee052241897272cb1072133a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b86e5adbe7264a2a4b8a35c8820b7a13",
+    "url": "/soundboard/index.html"
+  },
+  {
+    "revision": "a45aeb84ef298278e09f",
+    "url": "/soundboard/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5442c98d1a56814caef6",
+    "url": "/soundboard/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a45aeb84ef298278e09f",
+    "url": "/soundboard/static/js/2.2f0ed64a.chunk.js"
+  },
+  {
+    "revision": "51459ba9cea87b6a576e8e949a89eb86",
+    "url": "/soundboard/static/js/2.2f0ed64a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5442c98d1a56814caef6",
+    "url": "/soundboard/static/js/main.1fb1c8b9.chunk.js"
+  },
+  {
+    "revision": "ec258e08d7d5ddaa8343",
+    "url": "/soundboard/static/js/runtime-main.aaab88a1.js"
+  },
+  {
+    "revision": "6be24c2dc45871cf072b6cfee71b0dfb",
+    "url": "/soundboard/static/media/man.6be24c2d.mp3"
+  }
+]);
